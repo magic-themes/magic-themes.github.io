51cf295f38ada1620c88e08af93f9a6e49db6b55
jascha <jascha@jaeh.at>
2020-03-07 04:31:47 +0100

    app: add hero, footer, branding

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,39 @@
 export const state = {
   title: '@magic-themes',
-  description: '@magic-themes. javascript css-in-js objects ready for usage in the @magic app.',
-  logotext: '@magic-themes',
-  logo: '/logo.png',
+  description: '@magic-themes. javascript css-in-js objects ready for usage in a @magic app.',
+
+  branding: ['@magic', '-themes'],
+
   menu: [
-    {
-      to: '/#gl-magic-themes',
-      text: 'magic-themes',
-      items: [{ to: '-li-docs', text: 'docs' }],
-    },
+    { to: '/#gl-magic-themes', text: 'themes' },
   ],
+
+  hero: {
+    title: 'themes',
+    description: 'styles for your @magic app.',
+  },
+
+  footer: {
+    one: {
+      title: '@magic',
+      before: ['themes', 'created by', 'Wizards & Witches'],
+    },
+
+    two: {
+      title: 'projects',
+      menu: [
+        { to: 'https://github.com/magic/', text: '@magic' },
+        { to: 'https://github.com/grundstein/', text: '@grundstein' },
+        { to: 'https://github.com/webboot/', text: '@webboot' },
+      ],
+    },
+
+    three: {
+      title: 'social',
+      menu: [
+        { to: 'https://npmjs.com/org/magic-themes/', text: 'npm' },
+        { to: 'https://github.com/magic-themes/', text: 'github' },
+      ],
+    },
+  },
 }
